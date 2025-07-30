9986127dc8bf69f8ebc0e97202741a236e3da1fc
ai.xyz.data <ai.xyz.data@gmail.com>
2025-07-29 22:14:27 -0300

    fix: corrige caminho do SefazView

--- fisher/web/src/views/sources/SefazView.vue
+++ fisher/web/src/views/sources/SefazView.vue
@@ -469,7 +469,7 @@
 </template>
 
 <script>
-import CanonikaViewTemplate from '../../../../shared/templates/CanonikaViewTemplate.vue'
+import CanonikaViewTemplate from '../../../../../shared/templates/CanonikaViewTemplate.vue'
 
 export default {
   name: 'SefazView',
