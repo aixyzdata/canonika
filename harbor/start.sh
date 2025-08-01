17e989f6ccd963129f6682db4eb059b42289e9da
ai.xyz.data <ai.xyz.data@gmail.com>
2025-08-01 15:44:34 -0300

    Remove microserviços para configuração como submódulos

--- harbor/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Iniciar nginx em background
-nginx
-
-# Iniciar a API FastAPI
-cd /app
-uvicorn api.main:app --host 0.0.0.0 --port 8000 --reload 
--- quarter/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Iniciar uvicorn em background
-uvicorn api.main:app --host 0.0.0.0 --port 8000 &
-
-# Iniciar nginx
-nginx -g 'daemon off;' 
